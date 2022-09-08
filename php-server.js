3c62d447fbb9bb1258344811cc64dcdfdbc299f3
Ashley Engle <ashley.engle@intouchsol.com>
2022-09-08 17:58:52 -0500

    Add new PHP package

--- php-server.js
+++ php-server.js
@@ -1,10 +1,26 @@
-async function runServer() {
-  const {default: phpServer} = await import('php-server');
-  // phpServer.default();
+// async function runServer() {
+//   const {default: phpServer} = await import('php-server');
+//   // phpServer.default();
 
-  const server = await phpServer();
-  console.log(server);
-  console.log(`PHP Server running at ${server.url}`);
-}
+//   const server = await phpServer();
+//   console.log(server);
+//   console.log(`PHP Server running at ${server.url}`);
+// }
 
-runServer();
+// runServer();
+
+var phpServer = require("node-php-server");
+ 
+// Create a PHP Server
+phpServer.createServer({
+    port: 8000,
+    hostname: '127.0.0.1',
+    base: '.',
+    keepalive: false,
+    open: false,
+    bin: 'php',
+    router: __dirname + '/server.php'
+});
+
+// Close server
+// phpServer.close();

c9eff716e4b998b13746371c0ca76f194ae0f762
Ashley E <poweroftheforce@gmail.com>
2022-09-08 20:48:59 -0500

    Removed dead code

--- php-server.js
+++ php-server.js
@@ -1,14 +1,3 @@
-// async function runServer() {
-//   const {default: phpServer} = await import('php-server');
-//   // phpServer.default();
-
-//   const server = await phpServer();
-//   console.log(server);
-//   console.log(`PHP Server running at ${server.url}`);
-// }
-
-// runServer();
-
 var phpServer = require("node-php-server");
  
 // Create a PHP Server
@@ -21,6 +10,3 @@ phpServer.createServer({
     bin: 'php',
     router: __dirname + '/server.php'
 });
-
-// Close server
-// phpServer.close();
